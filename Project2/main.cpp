8126aba9e06de752145c76b081f562283a1cdeb9
tinghaoyei <tinghaoyei>
2021-05-01 17:53:17 +0800

    add event handler example

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -8,8 +8,17 @@ void setcolor(WORD color);
 void setForeGroundAndBackGroundColor(int ForeGroundColor, int BackGroundColor);
 void clrscr();
 
+HANDLE hStdin;
+DWORD fdwSaveOldMode;
+
+VOID ErrorExit(LPSTR);
+VOID KeyEventProc(KEY_EVENT_RECORD);
+VOID MouseEventProc(MOUSE_EVENT_RECORD);
+VOID ResizeEventProc(WINDOW_BUFFER_SIZE_RECORD);
+
 int main()
 {
+
 	const unsigned char CASTLE[5] = {
 		0b0101010,
 		0b0111110,
@@ -125,7 +134,26 @@ int main()
 		}
 	}
 
-	while (cin.get())
+	DWORD cNumRead, fdwMode, i;
+	INPUT_RECORD irInBuf[128];
+	int counter = 0;
+
+	// Get the standard input handle.
+
+	hStdin = GetStdHandle(STD_INPUT_HANDLE);
+	if (hStdin == INVALID_HANDLE_VALUE)
+		ErrorExit(LPSTR("GetStdHandle"));
+
+	// Save the current input mode, to be restored on exit.
+
+	if (!GetConsoleMode(hStdin, &fdwSaveOldMode))
+		ErrorExit(LPSTR("GetConsoleMode"));
+
+	// Enable the window and mouse input events.
+	fdwMode = ENABLE_EXTENDED_FLAGS | ENABLE_WINDOW_INPUT | ENABLE_MOUSE_INPUT;
+	if (!SetConsoleMode(hStdin, fdwMode))
+		ErrorExit(LPSTR("SetConsoleMode"));
+	while (true)
 	{
 		for (int i = 0; i < 56; i++) {
 			for (int j = 0; j < 104; j++) {
@@ -139,7 +167,49 @@ int main()
 			std::cout << std::endl;
 		}
 		setcolor(7);// normal
+		gotoxy(0, 0);
+
+		if (!ReadConsoleInput(
+			hStdin,      // input buffer handle
+			irInBuf,     // buffer to read into
+			128,         // size of read buffer
+			&cNumRead)) // number of records read
+			ErrorExit(LPSTR("ReadConsoleInput"));
+
+		// Dispatch the events to the appropriate handler.
+
+		for (i = 0; i < cNumRead; i++)
+		{
+			switch (irInBuf[i].EventType)
+			{
+			case KEY_EVENT: // keyboard input
+				KeyEventProc(irInBuf[i].Event.KeyEvent);
+				break;
+
+			case MOUSE_EVENT: // mouse input
+				MouseEventProc(irInBuf[i].Event.MouseEvent);
+				break;
+
+			case WINDOW_BUFFER_SIZE_EVENT: // scrn buf. resizing
+				ResizeEventProc(irInBuf[i].Event.WindowBufferSizeEvent);
+				break;
+
+			case FOCUS_EVENT:  // disregard focus events
+
+			case MENU_EVENT:   // disregard menu events
+				break;
+
+			default:
+				ErrorExit(LPSTR("Unknown event type"));
+				break;
+			}
+		}
+
+
 	}
+	// Restore input mode on exit.
+	SetConsoleMode(hStdin, fdwSaveOldMode);
+
 	return 0;
 }
 
@@ -178,4 +248,73 @@ void clrscr()
 	FillConsoleOutputAttribute(hConsole, csbi.wAttributes, dwConSize, coordScreen, &cCharsWritten);
 	SetConsoleCursorPosition(hConsole, coordScreen);
 	return;
+}
+
+VOID ErrorExit(LPSTR lpszMessage)
+{
+	fprintf(stderr, "%s\n", lpszMessage);
+
+	// Restore input mode on exit.
+
+	SetConsoleMode(hStdin, fdwSaveOldMode);
+
+	ExitProcess(0);
+}
+
+VOID KeyEventProc(KEY_EVENT_RECORD ker)
+{
+	printf("Key event: ");
+
+	if (ker.bKeyDown)
+		printf("key pressed\n");
+	else printf("key released\n");
+}
+
+VOID MouseEventProc(MOUSE_EVENT_RECORD mer)
+{
+#ifndef MOUSE_HWHEELED
+#define MOUSE_HWHEELED 0x0008
+#endif
+	//printf("Mouse event: ");
+
+	switch (mer.dwEventFlags)
+	{
+	case 0:
+		
+		if (mer.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED)
+		{
+			printf("left button press \n");
+		}
+		else if (mer.dwButtonState == RIGHTMOST_BUTTON_PRESSED)
+		{
+			printf("right button press \n");
+		}
+		else
+		{
+			printf("button press\n");
+		}
+		break;
+	case DOUBLE_CLICK:
+		printf("double click\n");
+		break;
+	case MOUSE_HWHEELED:
+		printf("horizontal mouse wheel\n");
+		break;
+	case MOUSE_MOVED:
+		printf("%d, %d\n", mer.dwMousePosition.X, mer.dwMousePosition.Y);
+		//printf("mouse moved\n");
+		break;
+	case MOUSE_WHEELED:
+		printf("vertical mouse wheel\n");
+		break;
+	default:
+		printf("unknown\n");
+		break;
+	}
+}
+
+VOID ResizeEventProc(WINDOW_BUFFER_SIZE_RECORD wbsr)
+{
+	printf("Resize event\n");
+	printf("Console screen buffer is %d columns by %d rows.\n", wbsr.dwSize.X, wbsr.dwSize.Y);
 }
